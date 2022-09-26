d87147c9180c76e40d9c26f380ff909a1730d052
Deleina <marta.deleina@gmail.com>
2022-09-26 11:09:32 -0500

    creando Login api, pages

---
+++ my-app/src/login-api/app.js
@@ -0,0 +1,56 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const cors = require('cors');
+
+const secret = "aBdshdbbAsdjjbdjdbjhbsabdnef6466164684"
+
+
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(cors())
+
+server.use((req, res, next) => {
+    console.log(req.headers);
+    if (req.method === "POST" && req.path === "/auth") {
+        next();
+    } else if (req.headers.authorization === `Bearer ${secret}`) {
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+})
+
+
+server.post("/auth", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (email == 'fmendoza@gmail.com' && password == '123456') {
+        const datos = {
+            id: "123",
+            nombre: "Felipe Mendoza",
+            email: "fmendoza@mail.com",
+            token: secret
+        };
+        res.status(200).json(datos);
+    } else {
+        res.status(400).send("Crendenciales incorrectas");
+    }
+});
+
+
+//    server.get("/options", (req, res)=>{
+//        const datos = [
+//            {id:1,cliente:"orden 1",total:2500 },
+//            {id:2,cliente:"orden 2",total:2100 },
+//            {id:3,cliente:"orden 3",total:200 }
+//         ];
+//         res.json(datos);
+//     });
+
+server.use(router)
+server.listen(3001, () => {
+    console.log('JSON Server is running')
+    console.log("servidor iniciado en el puerto 3001")
+})
