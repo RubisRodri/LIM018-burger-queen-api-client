3daafbc8464e1b4d9d58beae393b955583350d43
RubisRodri <rubisrodri@gmail.com>
2022-10-10 09:48:48 -0500

    añadiendo css mesas activas

---
+++ my-app/src/Pages/Waiter-ordenes/ActiveOrder.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar } from "../../Components/navbar/Navbar";
+import { Navigate } from "react-router-dom";
+import Footer from "../../Components/footer/footer";
+import mesas from "../../Pictures/mesas.png"
+import './ActiveOrder.css';
+
+export const ActiveOrder = () =>{
+   
+   
+   
+   
+    return(
+        <>
+        <Navbar />
+        <div className="contenedor-mesas">
+        <div className="mesas-activas">mesa 1<img src={mesas} className="table" /></div>
+        <div className="mesas-activas">mesa 2<img src={mesas} className="table"/></div>
+        <div className="mesas-activas">mesa 3<img src={mesas} className="table"/></div>
+        <div className="mesas-activas">mesa 4<img src={mesas} className="table"/></div>
+        <div className="mesas-activas">mesa 5<img src={mesas} className="table"/></div>
+        <div className="mesas-activas">mesa 6<img src={mesas} className="table"/></div>
+        </div>
+        <Footer />
+        </>
+    )
+}
