6d3cbce505c806cf99a2248f03e141111fc67a63
Deleina <marta.deleina@gmail.com>
2022-11-07 18:36:46 -0500

    admin, showProduct

--- my-app/src/Pages/Admin/Admin.jsx
+++ my-app/src/Pages/Admin/Admin.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Navbar } from '../../Components/navbar/Navbar.jsx';
 import Footer from '../../Components/footer/footer.jsx';
 import { useNavigate } from "react-router-dom";
+import agregar from '../../Pictures/agregar.png'
 import './admin.css';
 
 
@@ -11,8 +12,8 @@ export const Admin = () => {
     const [products, setProducts] = useState([]);
     const [user, setUser] = useState([]);
     const [isShown, setIsShown] = useState(true);
-    const [isShownEmployes, setIsShownEmplyes] = useState(true);
     const [curentSelectedproduct, setCurentSelectedproduct] = useState({});
+
 
     useEffect(() => {
         fetch('http://localhost:3001/products', {
@@ -25,9 +26,6 @@ export const Admin = () => {
             .then(response => response.json())
             .then((value) => {
                 setProducts(value)
-
-
-
             })
     }, [])
 
@@ -36,41 +34,11 @@ export const Admin = () => {
     const showProduct = (product) => {
         setCurentSelectedproduct(product);
         setIsShown(current => !current);
-        console.log(curentSelectedproduct);
-    }
-
-    const showEmployess = () => {
-        navigate("/Employess")
-        console.log("navegar a empleados")
-    }
-
-    useEffect(() => {
-        fetch('http://localhost:3001/users', {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json",
-                "Authorization": `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-            .then(response => response.json())
-            .then((value) => {
-                setUser(value)
-            })
-    }, [])
-
-
-
-    const showEmployes = (users) => {
-        setIsShownEmplyes(current => !current);
     }
 
 
-    const deleteProduct = (product) => {
-        const filterProducts = product.filter((value) => {
-            return value.id !== product.id
-        }); 
-
-        console.log( filterProducts );
+    const deleteProduct = (id) => {
+        
  
 
     }
@@ -84,7 +52,7 @@ export const Admin = () => {
             <div className="content">
                 <div className='container-btn-admin'>
                     <button type='button' className='products-btn'>Productos</button>
-                    <button type='button' className='employes-btn' onClick={() => showEmployes(user)}>Empleados</button>
+                    <button type='button' className='employes-btn' >Empleados</button>
                 </div>
                 <div className="add-admin">
                     <img src={agregar} className="add-btn-admin" />
@@ -108,33 +76,11 @@ export const Admin = () => {
                     </div>
                 </div>
             </div>
-
-            <div className="container-list-employes" style={{ display: isShownEmployes ? 'none' : 'block' }}>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Rol</th>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                            <th>Correo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {user.map((user, index) => {
-                            return (
-                                <tr key={user.id}>
-                                    <td>{user.roles}</td>
-                                    <td>{user.name}</td>
-                                    <td>{user.lastName}</td>
-                                    <td>{user.email}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
             <Footer />
         </>
 
     )
 }
+
+
+
--- my-app/src/login-api/app.js
+++ my-app/src/login-api/app.js
@@ -24,7 +24,7 @@ server.use((req, res, next) => {
         }
         next();
     } else {
-        
+
         res.sendStatus(401);
     }
 });
@@ -56,8 +56,8 @@ server.post("/auth", (req, res) => {
         return
     }
 
-// eslint-disable-next-line default-case
-switch (req.body.email) {
+    // eslint-disable-next-line default-case
+    switch (req.body.email) {
         case 'fmendoza@gmail.com':
             res.jsonp({
                 token: tokenWaiter
@@ -68,7 +68,7 @@ switch (req.body.email) {
             res.jsonp({
                 token: tokenCheff
             });
-             console.log("Cheff");
+            console.log("Cheff");
             break;
         case 'deleina@gmail.com':
             res.jsonp({
@@ -185,10 +185,8 @@ server.patch("/orders/:id", async (req, res) => {
 
 server.delete("/users/:id", async (req, res) => {
     const userId = req.params.id
-    //console.log("hola", userId)
     const changes = req.body
     const userP = router.db.get('users');
-    //console.log("holaaaa", userP)
     const user = userP.__wrapped__.users.find(el => el.id === userId)// orden que consiguio
     console.log("holaaaaa", user)
     if (!user) {
@@ -198,18 +196,13 @@ server.delete("/users/:id", async (req, res) => {
     userP.__wrapped__.users = userP.__wrapped__.users.filter(value => {
         return value.id !== userId
     });
-    // const orderUpdated = {
-    //     ...user,
-    //     ...changes
-    // }
-    // //userP.__wrapped__.users.push(orderUpdated);
     await userP.write();
     res.status(200).send(userP)
 })
 
 server.patch("/users/:id", async (req, res) => {
     const userId = req.params.id
-    //console.log("hola", userId)
+    console.log("hola", userId)
     const changes = req.body
     const userP = router.db.get('users');
     //console.log("holaaaa", userP)
@@ -230,6 +223,34 @@ server.patch("/users/:id", async (req, res) => {
     await userP.write();
     res.status(200).send(userUpdated)
 })
+
+
+server.delete("/products/:id", async (req, res) => {
+    const productId = req.params.id
+    const changes = req.body
+    console.log('product id',productId)
+    const productP = router.db.get('products');
+    console.log('product p',productP)
+
+    const product = productP.__wrapped__.products.find(value => value.id !== productId)
+
+    console.log('product...',product);
+    if (!product) {
+        res.status(404).send("Not found");
+        return
+    }
+    productP.__wrapped__.products = productP.__wrapped__.products.filter(value => {
+            return value.id !== productId
+        });
+    
+   
+    await productP.write();
+    res.status(200).send(productP)
+})
+
+
+
+
 
 
 server.use(router)
